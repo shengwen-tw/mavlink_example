17ba41ee1830ea65a8d711ff4c842fbe83d9a3ff
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-08-11 01:59:08 +0800

    Add command_long handler function

--- mavlink_parser.c
+++ mavlink_parser.c
@@ -4,22 +4,22 @@
 extern bool verbose;
 
 void mav_heartbeat(mavlink_message_t *recvd_msg);
+void mav_command_long(mavlink_message_t *recvd_msg);
 
-/* clang-format off */
 enum ENUM_MAV_CMDS {
     ENUM_MAVLINK_HANDLER(mav_heartbeat),
+    ENUM_MAVLINK_HANDLER(mav_command_long),
     MAV_CMD_CNT
 };
-/* clang-format on */
 
 struct mavlink_cmd cmd_list[] = {
     DEF_MAVLINK_CMD(mav_heartbeat, 0),
+    DEF_MAVLINK_CMD(mav_command_long, 76),
 };
 
 void parse_mavlink_msg(mavlink_message_t *msg)
 {
-    int i;
-    for (i = 0; i < (signed int) CMD_LEN(cmd_list); i++) {
+    for (int i = 0; i < CMD_LEN(cmd_list); i++) {
         if (msg->msgid == cmd_list[i].msg_id) {
             cmd_list[i].handler(msg);
             return;
@@ -34,4 +34,26 @@ void mav_heartbeat(mavlink_message_t *recvd_msg)
 {
     if (verbose)
         printf("[INFO] received heartbeat message.\n");
+}
+
+void mav_command_long(mavlink_message_t *recvd_msg)
+{
+    if (verbose)
+        printf("[INFO] received command_long message.\n");
+
+    /* decode command_long message */
+    mavlink_command_long_t mav_cmd_long;
+    mavlink_msg_command_long_decode(recvd_msg, &mav_cmd_long);
+
+    switch (mav_cmd_long.command) {
+    case MAV_CMD_DO_SET_ROI_LOCATION: /* 195 */
+        // mav_cmd_do_set_roi_location_handler(&mav_cmd_long);
+        break;
+    case MAV_CMD_DO_SET_ROI_NONE: /* 197 */
+        // mav_cmd_do_set_roi_none_handler(&mav_cmd_long);
+        break;
+    case MAV_CMD_REQUEST_MESSAGE: /* 512 */
+        // mav_cmd_request_msg_handler(&mav_cmd_long);
+        break;
+    }
 }
